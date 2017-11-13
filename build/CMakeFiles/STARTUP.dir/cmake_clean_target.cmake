59a66f2fddbdcb9c33a2aeb9594d25d8b445dd65
Tyler Barkin <Tybarkin@gmail.com>
2017-11-12 23:18:46 -0500

    Removed Build...

--- build/CMakeFiles/STARTUP.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "lib/libSTARTUP.a"
-)
--- build/CMakeFiles/burn.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/burn"
-  "burn-command"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/burn.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
